c9f16571dccc93dce169b8c0f5ff11e5ef124cd4
黎江樑 <47838732+laneston@users.noreply.github.com>
2023-03-10 09:42:19 +0800

    20230310
    修改cmake内容

--- cmake/nng.cmake
+++ cmake/nng.cmake
@@ -16,26 +16,34 @@ SET(DOWNLOAD_NNG_TIMEOUT 300 CACHE STRING "Timeout in seconds when downloading n
 
 MESSAGE(STATUS "Checking ${NNG_TAR_GZ_NAME}...")
 
-# 判断压缩包是否已经存在
+# 获取压缩包变量值
 FIND_FILE(LOCAL_NNG_TAR_GZ
 	NAMES ${NNG_TAR_GZ_NAME}
 	HINTS "${PROJECT_SOURCE_DIR}/libraries_path"
 	NO_DEFAULT_PATH)
 
-# 判断压缩包在文件夹libraries_path下是否已经解压
+# 获取文件变量值
 FIND_FILE(LOCAL_NNG_DIR
 	NAMES ${NNG_DIR_NAME}
 	HINTS "${PROJECT_SOURCE_DIR}/libraries_path"
 	NO_DEFAULT_PATH)
 
+# 获取压缩包路径值
 FIND_FILE(LOCAL_LIBRARIES_PATH
 	NAMES ""
 	HINTS "libraries_path"
 	NO_DEFAULT_PATH)
 
-MESSAGE(STATUS "Checking url link ${NNG_DOWNLOAD_URL} ...")
 
+
+
+
+
+
+# 判断压缩包是否已下载，如无下载，则进行下载
 IF(NOT LOCAL_NNG_TAR_GZ)
+
+	MESSAGE(STATUS "Checking url link ${NNG_DOWNLOAD_URL} ...")
 	MESSAGE(STATUS "Downloading ${NNG_TAR_GZ_NAME} to ${LOCAL_LIBRARIES_PATH} ...")
 	# 从设定的 URL 地址下载相应的压缩包
 	FILE(DOWNLOAD ${NNG_DOWNLOAD_URL}
@@ -53,42 +61,56 @@ IF(NOT LOCAL_NNG_TAR_GZ)
 		MESSAGE(STATUS "Download failed, error: ${ERR}")
 		MESSAGE(FATAL_ERROR
 		"You can try downloading ${NNG_DOWNLOAD_URL} manually"
-		" using curl/wget or a similar tool")
+		"using curl/wget or a similar tool")
 	ENDIF()
 	
 ENDIF()
 
-IF(LOCAL_NNG_TAR_GZ)
 
-	# 输出相应的提示信息校验解压路径等
-	MESSAGE(STATUS "Checking the direction of ${LOCAL_LIBRARIES_PATH}/${NNG_DIR_NAME}")
-	IF(NOT EXISTS "${LOCAL_LIBRARIES_PATH}/${NNG_DIR_NAME}")
-		# 输出提示信息准备进行解压缩
-		MESSAGE(STATUS "cd ${LOCAL_LIBRARIES_PATH} && tar -zxvf ${NNG_TAR_GZ_NAME}")
-		# 获取已下载的压缩包大小做基本的判断
-		FILE(SIZE ${LOCAL_NNG_TAR_GZ} NNG_TAR_GZ_SIZE)
-		MESSAGE(STATUS "${LOCAL_NNG_TAR_GZ} size is ${NNG_TAR_GZ_SIZE}")
 
-		# 如果压缩包大小为0则输出对应的错误信息
-		IF(${NNG_TAR_GZ_SIZE} EQUAL 0)
-			# 移除损坏压缩包，重置相应的参数
-			FILE(REMOVE ${LOCAL_NNG_TAR_GZ})
-	 		MESSAGE(FATAL_ERROR "${NNG_TAR_GZ_SIZE} is zero length. it had been deleted.")
-		# 下载文件无问题，进入解压路径开始解压压缩包
-		ELSE()
-			# 解压文件
-    		EXECUTE_PROCESS(COMMAND tar -zxvf ${LOCAL_NNG_TAR_GZ} -C ${LOCAL_LIBRARIES_PATH}/
-							WORKING_DIRECTORY "${LOCAL_LIBRARIES_PATH}"
-							RESULT_VARIABLE 10
-							RESULT_VARIABLE RESULT_TAR)
-			IF(RESULT_TAR MATCHES 0)
-				MESSAGE(STATUS "tar -zxvf ${LOCAL_NNG_TAR_GZ} -C ${LOCAL_LIBRARIES_PATH}/")
+IF(LOCAL_NNG_DIR)
+
+	MESSAGE(STATUS "${LOCAL_NNG_DIR} IS EXIST.")
+
+ELSE(LOCAL_NNG_DIR)
+
+	IF(LOCAL_NNG_TAR_GZ)
+
+		# 输出相应的提示信息校验解压路径等
+		MESSAGE(STATUS "Checking the direction of ${LOCAL_LIBRARIES_PATH}/${NNG_DIR_NAME}")
+		IF(NOT EXISTS "${LOCAL_LIBRARIES_PATH}/${NNG_DIR_NAME}")
+			# 输出提示信息准备进行解压缩
+			MESSAGE(STATUS "cd ${LOCAL_LIBRARIES_PATH} && tar -zxvf ${NNG_TAR_GZ_NAME}")
+			# 获取已下载的压缩包大小做基本的判断
+			FILE(SIZE ${LOCAL_NNG_TAR_GZ} NNG_TAR_GZ_SIZE)
+			MESSAGE(STATUS "${LOCAL_NNG_TAR_GZ} size is ${NNG_TAR_GZ_SIZE}")
+
+			# 如果压缩包大小为0则输出对应的错误信息
+			IF(${NNG_TAR_GZ_SIZE} EQUAL 0)
+				# 移除损坏压缩包，重置相应的参数
+				FILE(REMOVE ${LOCAL_NNG_TAR_GZ})
+				MESSAGE(FATAL_ERROR "${NNG_TAR_GZ_SIZE} is zero length. it had been deleted.")
+			# 下载文件无问题，进入解压路径开始解压压缩包
 			ELSE()
-				MESSAGE(FATAL_ERROR "[ERROR] tar -zxvf ${LOCAL_NNG_TAR_GZ} -C ${LOCAL_LIBRARIES_PATH}/ failed.\n")
+				# 解压文件
+				EXECUTE_PROCESS(COMMAND tar -zxvf ${LOCAL_NNG_TAR_GZ} -C ${LOCAL_LIBRARIES_PATH}/
+								WORKING_DIRECTORY "${LOCAL_LIBRARIES_PATH}"
+								RESULT_VARIABLE 10
+								RESULT_VARIABLE RESULT_TAR)
+				IF(RESULT_TAR MATCHES 0)
+					MESSAGE(STATUS "tar -zxvf ${LOCAL_NNG_TAR_GZ} -C ${LOCAL_LIBRARIES_PATH}/")
+				ELSE()
+					MESSAGE(FATAL_ERROR "[ERROR] tar -zxvf ${LOCAL_NNG_TAR_GZ} -C ${LOCAL_LIBRARIES_PATH}/ failed.\n")
+				ENDIF()
+				UNSET(RESULT_TAR)
 			ENDIF()
-			UNSET(RESULT_TAR)
+
 		ENDIF()
 
 	ENDIF()
 
-ENDIF()
+ENDIF(LOCAL_NNG_DIR)
+
+
+
+
--- doc/compile.md
+++ doc/compile.md
@@ -9,6 +9,11 @@ cmake .. -DCMAKE_TOOLCHAIN_FILE=../toolchain/toolchain_mtk.cmake
 ```
 
 
+```
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../toolchain/toolchain_mtk.cmake -DNNG_TESTS=OFF -DBUILD_SHARED_LIBS=OFF
+```
+
+
 # 是否编译动态库
 
 如果需要将 NNG/MQTT 编译成动态库，则需要使能 BUILD_SHARED_LIBS 参数。
